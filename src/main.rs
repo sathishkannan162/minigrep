0baf6174f37e29810280c3846099d446124f1473
Sathish Kannan <sathishkannan162@gmail.com>
2024-01-09 21:40:37 +0530

    Simple minigrep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::process;
+use std::env;
+
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem processing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application Error: {e}");
+        process::exit(1);
+    }
 }
+
