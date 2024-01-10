ec65ddd2ff11627f4cdd4cfab499f500b09583db
Sathish Kannan <sathishkannan162@gmail.com>
2024-01-11 02:57:54 +0530

    Use iterators over loops

--- src/lib.rs
+++ src/lib.rs
@@ -24,19 +24,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments.");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        let mut ignore_case = false;
-        if args.len() == 4 {
-            println!("Search is case insensitive.");
-            ignore_case = true;
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Enter a query string."),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Enter file path."),
+        };
 
-        let ignore_case = env::var("IGNORE_CASE").is_ok() || ignore_case;
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
             query,
@@ -47,13 +46,10 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
